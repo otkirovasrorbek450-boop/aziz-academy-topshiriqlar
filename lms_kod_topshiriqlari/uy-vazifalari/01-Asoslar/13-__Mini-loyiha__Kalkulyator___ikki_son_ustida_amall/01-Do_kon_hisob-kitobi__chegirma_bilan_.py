c1d7e1629ffae366ba2596729002acb8f26e9f5c
otkirovasrorbek450-boop <otkirovasrorbek450@gmail.com>
2026-07-06 10:05:05 +0500

    Add: Do'kon hisob-kitobi (chegirma bilan) [otkirovasrorbek450-boop]

---
+++ lms_kod_topshiriqlari/uy-vazifalari/01-Asoslar/13-__Mini-loyiha__Kalkulyator___ikki_son_ustida_amall/01-Do_kon_hisob-kitobi__chegirma_bilan_.py
@@ -0,0 +1,10 @@
+son = int(input().strip())
+narxi = int(input().strip())
+foiz = int(input().strip())
+umumiy = son * narxi
+chegirma = (umumiy * foiz) // 100
+yakuniy = umumiy - chegirma
+
+print(umumiy)
+print(chegirma)
+print(yakuniy)
