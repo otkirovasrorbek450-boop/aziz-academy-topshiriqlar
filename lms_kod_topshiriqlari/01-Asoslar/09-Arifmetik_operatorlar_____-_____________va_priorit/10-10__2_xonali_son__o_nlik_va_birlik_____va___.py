592c4f09723ec59b16c22b73e9a2efd569653bb7
otkirovasrorbek450-boop <otkirovasrorbek450@gmail.com>
2026-08-04 06:25:31 +0300

    Add: 10) 2 xonali son: o‘nlik va birlik (// va %) [otkirovasrorbek450-boop]

--- lms_kod_topshiriqlari/01-Asoslar/09-Arifmetik_operatorlar_____-_____________va_priorit/10-10__2_xonali_son__o_nlik_va_birlik_____va___.py
+++ lms_kod_topshiriqlari/01-Asoslar/09-Arifmetik_operatorlar_____-_____________va_priorit/10-10__2_xonali_son__o_nlik_va_birlik_____va___.py
@@ -1,5 +1,11 @@
-n = int(input())
-tens = n // 10
-ones = n % 10
-print(f"Tens: {tens}")
-print(f"Ones: {ones}")
+try:
+    n = int(input())
+    if 10 <= n <= 99:
+        tens = n // 10
+        ones = n % 10
+        print(f"Tens: {tens}")
+        print(f"Ones: {ones}")
+    else:
+        print("BAD")
+except:
+    print("BAD")
