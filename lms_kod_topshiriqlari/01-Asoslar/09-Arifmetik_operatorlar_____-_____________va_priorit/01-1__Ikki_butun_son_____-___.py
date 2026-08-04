f0c30e6a2ff64ecd17e8a47addea089eae3e456c
otkirovasrorbek450-boop <otkirovasrorbek450@gmail.com>
2026-08-04 06:24:16 +0300

    Add: 1) Ikki butun son: +, -, * [otkirovasrorbek450-boop]

--- lms_kod_topshiriqlari/01-Asoslar/09-Arifmetik_operatorlar_____-_____________va_priorit/01-1__Ikki_butun_son_____-___.py
+++ lms_kod_topshiriqlari/01-Asoslar/09-Arifmetik_operatorlar_____-_____________va_priorit/01-1__Ikki_butun_son_____-___.py
@@ -1,4 +1,12 @@
-a, b = map(int, input().split())
-print(f"Add: {a + b}")
-print(f"Sub: {a - b}")
-print(f"Mul: {a * b}")
+a, b = input().split()
+a = int(a)
+b = int(b)
+print("Add:", a + b)
+print("Sub:", a - b)
+print("Mul:", a * b)
+
+
+# a va b (bitta qatorda)
+# 1-qator: "Add: <a+b>"
+# 2-qator: "Sub: <a-b>"
+# 3-qator: "Mul: <a*b>"
