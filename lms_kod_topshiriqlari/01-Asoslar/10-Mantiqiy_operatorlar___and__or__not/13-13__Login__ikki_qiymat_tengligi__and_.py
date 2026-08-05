cecb9e7132f9cd99338ac67bf872d00aaa5439d1
otkirovasrorbek450-boop <otkirovasrorbek450@gmail.com>
2026-08-05 06:20:30 +0300

    Add: 13) Login: ikki qiymat tengligi (and) [otkirovasrorbek450-boop]

--- lms_kod_topshiriqlari/01-Asoslar/10-Mantiqiy_operatorlar___and__or__not/13-13__Login__ikki_qiymat_tengligi__and_.py
+++ lms_kod_topshiriqlari/01-Asoslar/10-Mantiqiy_operatorlar___and__or__not/13-13__Login__ikki_qiymat_tengligi__and_.py
@@ -1,4 +1,3 @@
 username = input()
-password = input()
-result = (username == "admin" and password == "1234")
-print(result)
+password = input()   
+print(username == "admin" and password == "1234")
