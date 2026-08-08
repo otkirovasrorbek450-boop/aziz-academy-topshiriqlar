3f5e8da85278509f7da69d8c82a61a238d242cae
otkirovasrorbek450-boop <otkirovasrorbek450@gmail.com>
2026-08-08 07:21:42 +0300

    Add: 9) Sekundni minut va sekundga ajratish [otkirovasrorbek450-boop]

--- lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/09-9__Sekundni_minut_va_sekundga_ajratish.py
+++ lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/09-9__Sekundni_minut_va_sekundga_ajratish.py
@@ -1,2 +1,8 @@
-m, s = divmod(int(input()), 60)
-print(f"Minut: {m}\nSekund: {s}")
+# Umumiy sekundni butun son sifatida kiriting.
+# Minut = sekund // 60
+# Qolgan sekund = sekund % 60
+# 1-qator: Minut: <m>
+# 2-qator: Sekund: <s>
+sekund = int(input())
+print("Minut:", sekund // 60)
+print("Sekund:", sekund % 60)
