2fc1f30db022f68dd59c430bdafd9c406f87223a
otkirovasrorbek450-boop <otkirovasrorbek450@gmail.com>
2026-08-08 07:21:47 +0300

    Add: 10) Turli tipdagi o‘zgaruvchilar [otkirovasrorbek450-boop]

--- lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/10-10__Turli_tipdagi_o_zgaruvchilar.py
+++ lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/10-10__Turli_tipdagi_o_zgaruvchilar.py
@@ -1,2 +1,8 @@
-name, age = input(), input()
-print(f"{name} {age} yoshda")
+# 1-qator: ism
+# 2-qator: yosh (butun son)
+# "<ism> <yosh> yoshda" ko'rinishida satr hosil qilib, uni chiqaring.
+# O'zgaruvchi nomlarini mazmunli qiling (masalan: name, age, message).
+name = input()
+age = input()
+message = name + " " + age + " yoshda "
+print(message)
