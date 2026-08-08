deb96b6b72587e5436531f2188453f2baa073d46
otkirovasrorbek450-boop <otkirovasrorbek450@gmail.com>
2026-08-08 07:20:24 +0300

    Add: 6) To'rtburchak yuzi va perimetri [otkirovasrorbek450-boop]

--- lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/06-6__To_rtburchak_yuzi_va_perimetri.py
+++ lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/06-6__To_rtburchak_yuzi_va_perimetri.py
@@ -1,2 +1,12 @@
-e, b = map(int, input().split())
-print(f"Yuza: {e*b}\nPerimetr: {(e+b)*2}")
+eni, boyi = map(int, input().split())
+yuza = eni * boyi
+perimetr = 2 * (eni + boyi)
+
+print(f"Yuza: {yuza}")
+print(f"Perimetr: {perimetr}")
+
+# Bir qatorda eni va bo'yi (butun son) kiriting.
+# Yuza = eni * bo'yi
+# Perimetr = 2 * (eni + bo'yi)
+# 1-qator: Yuza: <yuza>
+# 2-qator: Perimetr: <perimetr>
