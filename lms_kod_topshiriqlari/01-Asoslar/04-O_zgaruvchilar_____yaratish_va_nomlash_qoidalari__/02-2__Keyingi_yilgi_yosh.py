343d37c80006387bb4d25d4fd51368ac018681ff
otkirovasrorbek450-boop <otkirovasrorbek450@gmail.com>
2026-08-08 07:19:57 +0300

    Add: 2) Keyingi yilgi yosh [otkirovasrorbek450-boop]

--- lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/02-2__Keyingi_yilgi_yosh.py
+++ lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/02-2__Keyingi_yilgi_yosh.py
@@ -1,2 +1,5 @@
-yosh = int(input())
-print(yosh + 1)
+x = int(input())
+print(x + 1)
+# Bitta butun yosh kiriting.
+# O'zgaruvchi nomlarini mazmunli qilib tanlab, keyingi yilgi yoshni (yosh + 1) hisoblab chiqaring.
+# Faqat natijani (butun son) chiqaring.
