3b5c9f8dfe710200f4e95d8b684c941dbcc712d9
otkirovasrorbek450-boop <otkirovasrorbek450@gmail.com>
2026-08-08 07:20:03 +0300

    Add: 3) Uchta son yig'indisi [otkirovasrorbek450-boop]

--- lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/03-3__Uchta_son_yig_indisi.py
+++ lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/03-3__Uchta_son_yig_indisi.py
@@ -1,1 +1,5 @@
-print("Yig'indi:", sum(map(int, input().split())))
+a, b, c =map(int, input().split())
+summa = a + b + c 
+print("Yig'indi:", summa)
+# Bir qatorda uchta butun son kiriting (probel bilan).
+# Ularning yig'indisini hisoblab, "Yig'indi: <summa>" ko'rinishida chiqaring.
