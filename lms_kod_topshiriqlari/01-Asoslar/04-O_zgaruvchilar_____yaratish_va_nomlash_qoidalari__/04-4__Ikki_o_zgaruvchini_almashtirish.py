b54d0896844137b37bf618d2dc5ec100a37650c0
otkirovasrorbek450-boop <otkirovasrorbek450@gmail.com>
2026-08-08 07:20:08 +0300

    Add: 4) Ikki o‘zgaruvchini almashtirish [otkirovasrorbek450-boop]

--- lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/04-4__Ikki_o_zgaruvchini_almashtirish.py
+++ lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/04-4__Ikki_o_zgaruvchini_almashtirish.py
@@ -1,2 +1,8 @@
-a, b = input().split()
-print(f"a={b}\nb={a}")
+a, b =map(int, input().split())
+a, b = b, a
+print(f"a={a}")
+print(f"b={b}")
+# Bir qatorda ikki butun son kiriting: a va b.
+# Ularning qiymatini o'zaro almashtiring va quyidagicha chiqaring:
+# a=<yangi_a>
+# b=<yangi_b>
