f02d1761814ecf5ab2bb7ff520eff4cb16fe4beb
otkirovasrorbek450-boop <otkirovasrorbek450@gmail.com>
2026-08-08 07:19:53 +0300

    Add: 1) Ism va yosh o‘zgaruvchilari [otkirovasrorbek450-boop]

--- lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/01-1__Ism_va_yosh_o_zgaruvchilari.py
+++ lms_kod_topshiriqlari/01-Asoslar/04-O_zgaruvchilar_____yaratish_va_nomlash_qoidalari__/01-1__Ism_va_yosh_o_zgaruvchilari.py
@@ -1,2 +1,8 @@
-print("Ism:", input())
-print("Yosh:", input())
+ism = input()
+yosh = input()
+
+print("Ism:", ism)
+print("Yosh:", yosh)
+# 1-qator: ism
+# 2-qator: yosh (butun son)
+# "Ism: <ism>" va "Yosh: <yosh>" ko'rinishida ikki qatorda chiqarib bering.
